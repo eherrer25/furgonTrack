d9031d09be35708ace727931aec022201825984c
= <=>
2019-11-18 16:00:11 -0300

    add menu conductor

--- src/app/menu-conductor/menu-conductor.page.ts
+++ src/app/menu-conductor/menu-conductor.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {NavController} from '@ionic/angular';
+import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
   selector: 'app-menu-conductor',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuConductorPage implements OnInit {
 
-  constructor() { }
+  userEmail: string;
+
+  constructor(private navCtrl: NavController,
+              private authService: AuthenticationService,
+              private router: Router) { }
 
   ngOnInit() {
+    if(this.authService.userDetails()){
+      this.userEmail = this.authService.userDetails().email;
+    }else{
+      this.navCtrl.navigateBack('');
+    }
+  }
+
+  navigateStudent() {
+    this.router.navigate(['students']);
   }
 
 }
